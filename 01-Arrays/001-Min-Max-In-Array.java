b2c43ec712089fad146558481e841e86b8e11a38
Sri Ram K <sriram2008k@gmail.com>
2026-07-19 19:35:38 +0530

    Update 001-Min-Max-In-Array.java

--- 01-Arrays/001-Min-Max-In-Array.java
+++ 01-Arrays/001-Min-Max-In-Array.java
@@ -1,3 +1,10 @@
+/*
+Problem: Min Max in Array
+Platform: GeeksforGeeks
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 class Solution {
     public ArrayList<Integer> getMinMax(int[] arr) {
         // code Here
